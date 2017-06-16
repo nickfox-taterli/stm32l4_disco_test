4f2a787c79b502c2557e0863faa9c98067645680
TaterLi <admin@lijingquan.net>
2017-06-16 21:21:16 +0800

    Signed-off-by: TaterLi <admin@lijingquan.net>

--- Middlewares/Third_Party/FreeRTOS-Plus-CLI/FreeRTOS_CLI.c
+++ Middlewares/Third_Party/FreeRTOS-Plus-CLI/FreeRTOS_CLI.c
@@ -206,7 +206,7 @@ size_t xCommandStringLength;
 	{
 		/* The command was found, but the number of parameters with the command
 		was incorrect. */
-		strncpy( pcWriteBuffer, "Incorrect command parameter(s).  Enter \"help\" to view a list of available commands.\r\n\r\n", xWriteBufferLen );
+		strncpy( pcWriteBuffer, "Incorrect command parameter(s).\r\n\r\n", xWriteBufferLen );
 		pxCommand = NULL;
 	}
 	else if( pxCommand != NULL )
@@ -225,7 +225,7 @@ size_t xCommandStringLength;
 	else
 	{
 		/* pxCommand was NULL, the command was not found. */
-		strncpy( pcWriteBuffer, "Command not recognised.  Enter 'help' to view a list of available commands.\r\n\r\n", xWriteBufferLen );
+		strncpy( pcWriteBuffer, "Command not recognised.\r\n\r\n", xWriteBufferLen );
 		xReturn = pdFALSE;
 	}
 
--- Middlewares/Third_Party/FreeRTOS-Plus-CLI/UARTCommandConsole.c
+++ Middlewares/Third_Party/FreeRTOS-Plus-CLI/UARTCommandConsole.c
@@ -111,8 +111,8 @@ void vUARTCommandConsoleStart( uint16_t usStackSize, UBaseType_t uxPriority );
 /*-----------------------------------------------------------*/
 
 /* Const messages output by the command console. */
-static const char * const pcWelcomeMessage = "FreeRTOS command server.\r\nType Help to view a list of registered commands.\r\n\r\n>";
-static const char * const pcEndOfOutputMessage = "\r\n[Press ENTER to execute the previous command again]\r\n>";
+static const char * const pcWelcomeMessage = "\r\n\r\nTaterLi CLI Server.\r\n\r\nTater-CLI>";
+static const char * const pcEndOfOutputMessage = "\r\nTater-CLI>";
 static const char * const pcNewLine = "\r\n";
 
 /* Used to guard access to the UART in case messages are sent to the UART from
